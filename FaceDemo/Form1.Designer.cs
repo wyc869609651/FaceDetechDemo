a73942e24c843f2378ffe66cca77ec933a91414f
anymore <869609651@qq.com>
2019-11-20 16:46:23 +0800

    添加项目文件。

---
+++ FaceDemo/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace FaceDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.textBoxDetection1 = new System.Windows.Forms.TextBox();
+            this.textBoxDetection2 = new System.Windows.Forms.TextBox();
+            this.textBoxCompare = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnLoadPic1 = new System.Windows.Forms.Button();
+            this.btnLoadPic2 = new System.Windows.Forms.Button();
+            this.btnCompare = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(346, 275);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox2.Location = new System.Drawing.Point(563, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(346, 275);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // textBoxDetection1
+            // 
+            this.textBoxDetection1.Font = new System.Drawing.Font("楷体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBoxDetection1.Location = new System.Drawing.Point(16, 355);
+            this.textBoxDetection1.Multiline = true;
+            this.textBoxDetection1.Name = "textBoxDetection1";
+            this.textBoxDetection1.ReadOnly = true;
+            this.textBoxDetection1.Size = new System.Drawing.Size(346, 238);
+            this.textBoxDetection1.TabIndex = 2;
+            // 
+            // textBoxDetection2
+            // 
+            this.textBoxDetection2.Font = new System.Drawing.Font("楷体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBoxDetection2.Location = new System.Drawing.Point(563, 355);
+            this.textBoxDetection2.Multiline = true;
+            this.textBoxDetection2.Name = "textBoxDetection2";
+            this.textBoxDetection2.ReadOnly = true;
+            this.textBoxDetection2.Size = new System.Drawing.Size(346, 238);
+            this.textBoxDetection2.TabIndex = 3;
+            // 
+            // textBoxCompare
+            // 
+            this.textBoxCompare.Font = new System.Drawing.Font("楷体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBoxCompare.Location = new System.Drawing.Point(381, 70);
+            this.textBoxCompare.Multiline = true;
+            this.textBoxCompare.Name = "textBoxCompare";
+            this.textBoxCompare.ReadOnly = true;
+            this.textBoxCompare.Size = new System.Drawing.Size(159, 86);
+            this.textBoxCompare.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("楷体", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(12, 333);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(129, 19);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "人脸检测结果";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("楷体", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(559, 333);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(129, 19);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "人脸检测结果";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("楷体", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(377, 48);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 19);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "对比结果";
+            // 
+            // btnLoadPic1
+            // 
+            this.btnLoadPic1.Font = new System.Drawing.Font("楷体", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnLoadPic1.Location = new System.Drawing.Point(283, 293);
+            this.btnLoadPic1.Name = "btnLoadPic1";
+            this.btnLoadPic1.Size = new System.Drawing.Size(75, 23);
+            this.btnLoadPic1.TabIndex = 1;
+            this.btnLoadPic1.Text = "载入图像";
+            this.btnLoadPic1.UseVisualStyleBackColor = true;
+            this.btnLoadPic1.Click += new System.EventHandler(this.btnLoadPic1_Click);
+            // 
+            // btnLoadPic2
+            // 
+            this.btnLoadPic2.Font = new System.Drawing.Font("楷体", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnLoadPic2.Location = new System.Drawing.Point(834, 293);
+            this.btnLoadPic2.Name = "btnLoadPic2";
+            this.btnLoadPic2.Size = new System.Drawing.Size(75, 23);
+            this.btnLoadPic2.TabIndex = 9;
+            this.btnLoadPic2.Text = "载入图像";
+            this.btnLoadPic2.UseVisualStyleBackColor = true;
+            this.btnLoadPic2.Click += new System.EventHandler(this.btnLoadPic2_Click);
+            // 
+            // btnCompare
+            // 
+            this.btnCompare.Font = new System.Drawing.Font("楷体", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnCompare.Location = new System.Drawing.Point(381, 12);
+            this.btnCompare.Name = "btnCompare";
+            this.btnCompare.Size = new System.Drawing.Size(75, 23);
+            this.btnCompare.TabIndex = 10;
+            this.btnCompare.Text = "人脸对比";
+            this.btnCompare.UseVisualStyleBackColor = true;
+            this.btnCompare.Click += new System.EventHandler(this.btnCompare_Click);
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(921, 603);
+            this.Controls.Add(this.btnCompare);
+            this.Controls.Add(this.btnLoadPic2);
+            this.Controls.Add(this.btnLoadPic1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxCompare);
+            this.Controls.Add(this.textBoxDetection2);
+            this.Controls.Add(this.textBoxDetection1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Font = new System.Drawing.Font("楷体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FaceDemo";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TextBox textBoxDetection1;
+        private System.Windows.Forms.TextBox textBoxDetection2;
+        private System.Windows.Forms.TextBox textBoxCompare;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnLoadPic1;
+        private System.Windows.Forms.Button btnLoadPic2;
+        private System.Windows.Forms.Button btnCompare;
+    }
+}
+
---
+++ FaceDemo/Form1.cs
@@ -0,0 +1,237 @@
+﻿using Baidu.Aip.Face;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FaceDemo
+{
+    public partial class Form1 : Form
+    {
+
+        private readonly string API_KEY = "I9ahRDhftvHEj2XkTLET5Ewi";
+
+        private readonly string SECRET_KEY = "PoKM8tMas3kjWZ98uBTTVDNjExcerhoX";
+
+        private Face client = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+            client = new Baidu.Aip.Face.Face(API_KEY, SECRET_KEY);
+        }
+
+
+        private void btnLoadPic1_Click(object sender, EventArgs e)
+        {
+            //选择图片
+            picPath1 = String.Empty;
+            OpenFileDialog fileDia = new OpenFileDialog();
+            fileDia.CheckFileExists = true;
+            fileDia.Filter = "图片(*.jpg;*.jpeg;*.png;*.bmp)|*.jpg;*.jpeg;*.png;*.bmp";
+            
+            if(fileDia.ShowDialog() == DialogResult.OK)
+            {
+                picPath1 = fileDia.FileName;
+            }
+            else
+            {
+                return;
+            }
+            pictureBox1.Load(picPath1);
+
+            //人脸检测
+            Dictionary<string, object> options = new Dictionary<string, object>();
+            options.Add("face_field", "age,beauty,expression,face_shape,gender,glassed,emotion,race,face_type");
+            JObject jObject = client.Detect(ImgToBase64(picPath1), "BASE64", options);
+
+            //解析结果
+            textBoxDetection1.Text = parseDetectResult(jObject, pictureBox1);
+        }
+
+        private void btnLoadPic2_Click(object sender, EventArgs e)
+        {
+            //选择图片
+            picPath2 = String.Empty;
+            OpenFileDialog fileDia = new OpenFileDialog();
+            fileDia.CheckFileExists = true;
+            fileDia.Filter = "图片(*.jpg;*.jpeg;*.png;*.bmp)|*.jpg;*.jpeg;*.png;*.bmp";
+
+            if (fileDia.ShowDialog() == DialogResult.OK)
+            {
+                picPath2 = fileDia.FileName;
+            }
+            else
+            {
+                return;
+            }
+            pictureBox2.Load(picPath2);
+
+            //人脸检测
+            Dictionary<string, object> options = new Dictionary<string, object>();
+            options.Add("face_field", "age,beauty,expression,face_shape,gender,glassed,emotion,race,face_type");
+            JObject jObject = client.Detect(ImgToBase64(picPath2), "BASE64", options);
+
+            //解析结果
+            textBoxDetection2.Text = parseDetectResult(jObject, pictureBox2);
+        }
+
+
+
+        /**
+         * 解析人脸检测结果
+         */
+        private string parseDetectResult(JObject jObject, PictureBox pictureBox) {
+            Dictionary<string, string> expressionDict = new Dictionary<string, string> { { "none", "不笑" }, { "smile", "微笑" }, { "laugh", "大笑" } };
+            Dictionary<string, string> faceShapeDict = new Dictionary<string, string> { { "square", "正方形" }, { "triangle", "三教形" }, { "oval", "椭圆" }, { "heart", "瓜子脸" }, { "round", "圆形" } };
+            Dictionary<string, string> genderDict = new Dictionary<string, string> { { "male", "男性" }, { "female", "女性" } };
+            Dictionary<string, string> glassesDict = new Dictionary<string, string> { { "none", "无眼镜" }, { "common", "普通眼镜" }, { "sun", "太阳镜"} };
+            Dictionary<string, string> emotionDict = new Dictionary<string, string> { { "angry", "愤怒" }, { "disgust", "厌恶" }, { "fear", "恐惧" }, 
+                                                                                    { "happy", "高兴" }, { "sad", "伤心" }, { "surprise", "惊讶" },
+                                                                                    { "neutral", "无表情" }, { "pouty", "噘嘴" }, { "grimace", "鬼脸" }};
+            Dictionary<string, string> raceDict = new Dictionary<string, string> { { "yellow", "黄种人" }, { "white", "白种人" }, { "black", "黑种人" }, { "arabs", "阿拉伯人" } };
+            Dictionary<string, string> faceTypeDict = new Dictionary<string, string> { { "human", "真实人脸" }, { "cartoon", "卡通人脸" }};
+
+
+            int errorCode = int.Parse(jObject.GetValue("error_code").ToString());
+            string text = String.Empty;
+            if (errorCode == 0)
+            {
+                JToken result = jObject.GetValue("result");
+                JArray faceList = result.Value<JArray>("face_list");
+                JToken faceItem = faceList.Children().First();
+                //标记人脸位置
+                JToken location = faceItem.Value<JToken>("location");
+                int left = location.Value<int>("left");
+                int top = location.Value<int>("top");
+                int width = location.Value<int>("width");
+                int height = location.Value<int>("height");
+                float rotation = location.Value<float>("rotation");
+                Graphics graphics = Graphics.FromImage(pictureBox.Image);
+                graphics.TranslateTransform(left, top);
+                graphics.RotateTransform(rotation);
+                graphics.DrawRectangle(new Pen(Color.Red, 2), new Rectangle(0, 0, width, height));
+                graphics.ResetTransform();
+                graphics.Save();
+
+                //显示检测结果
+                int age = faceItem.Value<int>("age");
+                double beauty = faceItem.Value<double>("beauty");
+                JToken expression = faceItem.Value<JToken>("expression");
+                JToken faceShape = faceItem.Value<JToken>("face_shape");
+                JToken gender = faceItem.Value<JToken>("gender");
+                JToken glasses = faceItem.Value<JToken>("glasses");
+                JToken emotion = faceItem.Value<JToken>("emotion");
+                JToken race = faceItem.Value<JToken>("race");
+                JToken faceType = faceItem.Value<JToken>("face_type");
+
+                text += "年龄：" + age + "\r\n";
+                text += "颜值：" + beauty + "\r\n";
+                if (expression != null && !expression.Value<String>("type").Equals(String.Empty))
+                {
+                    text += "表情：" + expressionDict[expression.Value<String>("type")] +"  置信度："+expression.Value<double>("probability") + "\r\n";
+                }
+                if (faceShape != null && !faceShape.Value<String>("type").Equals(String.Empty))
+                {
+                    text += "脸型：" + faceShapeDict[faceShape.Value<String>("type")] + "  置信度：" + faceShape.Value<double>("probability") + "\r\n";
+                }
+                if (gender != null && !gender.Value<String>("type").Equals(String.Empty))
+                {
+                    text += "性别：" + genderDict[gender.Value<String>("type")] + "  置信度：" + gender.Value<double>("probability") + "\r\n";
+                }
+                if (glasses != null && !glasses.Value<String>("type").Equals(String.Empty))
+                {
+                    text += "眼镜：" + glassesDict[glasses.Value<String>("type")] + "  置信度：" + glasses.Value<double>("probability") + "\r\n";
+                }
+                if (emotion!=null && !emotion.Value<String>("type").Equals(String.Empty))
+                {
+                    text += "情绪：" + emotionDict[emotion.Value<String>("type")] + "  置信度：" + emotion.Value<double>("probability") + "\r\n";
+                }
+                if (race != null && !race.Value<String>("type").Equals(String.Empty))
+                {
+                    text += "人种：" + raceDict[race.Value<String>("type")] + "  置信度：" + race.Value<double>("probability") + "\r\n";
+                }
+                if (faceType != null && !faceType.Value<String>("type").Equals(String.Empty))
+                {
+                    text += "类别：" + faceTypeDict[faceType.Value<String>("type")] + "  置信度：" + faceType.Value<double>("probability");
+                }
+            }
+            else
+            {
+                text = jObject.GetValue("error_msg").ToString();
+            }
+            return text;
+        }
+
+        /**
+         * 图片转base64
+         */
+        private string ImgToBase64(string path)
+        {
+            try
+            {
+                return Convert.ToBase64String(File.ReadAllBytes(path));
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        private string picPath1 = String.Empty;
+        private string picPath2 = String.Empty;
+        private void btnCompare_Click(object sender, EventArgs e)
+        {
+            if(picPath1.Equals(String.Empty) || picPath2.Equals(String.Empty))
+            {
+                MessageBox.Show("请载入两幅图片！");
+                return;
+            }           
+            var faces = new JArray
+                {
+                    new JObject
+                    {
+                        {"image", ImgToBase64(picPath1)},
+                        {"image_type", "BASE64"},
+                        {"quality_control", "LOW"},
+                    },
+                    new JObject
+                    {
+                        {"image", ImgToBase64(picPath2)},
+                        {"image_type", "BASE64"},
+                        {"quality_control", "LOW"},
+                    }
+                };
+            JObject jObject = client.Match(faces);
+            int errorCode = int.Parse(jObject.GetValue("error_code").ToString());
+            string text = String.Empty;
+            if (errorCode == 0)
+            {
+                JToken result = jObject.GetValue("result");
+                float score = result.Value<float>("score");
+                if (score >= 80)
+                {
+                    text += "同一个人\r\n";
+                }
+                else
+                {
+                    text += "不是同一个人\r\n";
+                }
+                text += "相似度：" + score;
+            }
+            else
+            {
+                text = jObject.GetValue("error_msg").ToString();
+            }
+            textBoxCompare.Text = text;
+        }
+    }
+}
